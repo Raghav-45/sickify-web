8ad722585bcf53dda7b72aecc7eee9ba94fb4990
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-04-13 15:19:03 +0530

    Create Layout.js

---
+++ components/Layout.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+import { Playback } from './Playback'
+
+import { AiFillHome } from 'react-icons/ai'
+import { FiSearch } from 'react-icons/fi'
+import { BiLibrary } from 'react-icons/bi'
+import { YTPlayer } from './YTPlayer'
+
+export default function Layout({ children }) {
+  return (
+    <div className='h-full bg-center'>
+
+      {children}
+
+      {/* <div className='h-[350px]'></div> */}
+
+      <footer className='bg-gradient-to-t from-dark backdrop-blur-lg text-white fixed inset-x-0 bottom-0'>
+        <Playback/>
+        <div className='grid grid-cols-3 gap-0 h-14 mx-5 text-xl text-center'>
+          <Link href='/home' className='flex h-full w-full align-middle' replace><AiFillHome className='m-auto'/></Link>
+          <Link href='/search' className='flex h-full w-full align-middle' replace><FiSearch className='m-auto'/></Link>
+          <Link href='/library' className='flex h-full w-full align-middle' replace><BiLibrary className='m-auto'/></Link>
+        </div>
+        <YTPlayer/>
+      </footer>
+    </div>
+  )
+}
