7be3c578dc4ef801d579be47077bd10a36be39cf
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-04-23 16:07:43 +0530

    added trending song in home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Layout from '../../components/Layout'
+import HomePage from './home'
 
 export default function Home() {
   return (
-    <Layout>
-      <div>HomePage</div>
-    </Layout>
+    <HomePage />
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ module.exports = {
     extend: {
       colors: {
         'dark': '#121212',
-        // 'Darkest': '#191414',
-        // 'light': '#282828',
-        // 'lightest': '#B3B3B3',
+        'Darkest': '#191414',
+        'light': '#282828',
+        'lightest': '#B3B3B3',
       },
     },
   },
