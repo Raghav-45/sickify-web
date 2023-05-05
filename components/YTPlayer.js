2b2a347742038970b287071efd937723cfffb042
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-05-05 21:37:15 +0530

    Update YTPlayer.js

--- components/YTPlayer.js
+++ components/YTPlayer.js
@@ -5,8 +5,15 @@ import ReactPlayer from 'react-player/youtube'
 export const YTPlayer = ((props) => {
   const { TrackData, MusicSourceURL, isPlaying, setIsPlaying, setMusicDuration, setMusicCurrentTime, setBufferDuration, IsBuffering, setIsBuffering } = PlayerContext()
 
+  // const opts = {
+  //   height: '24px',
+  //   width: '100%',
+  //   playerVars: {
+  //     autoplay: 1,
+  //   },
+  // }
   const opts = {
-    height: '24px',
+    height: '100%',
     width: '100%',
     playerVars: {
       autoplay: 1,
@@ -19,7 +26,7 @@ export const YTPlayer = ((props) => {
 
   return (
     // Load the YouTube player
-    <ReactPlayer style={{display: 'none'}} url={`https://music.youtube.com/watch?v=${TrackData.YTid}`} height={opts.height} width={opts.width} playing={isPlaying} controls={false} onBuffer={setIsBuffering(true)} onBufferEnd={setIsBuffering(false)} onDuration={(e) => setMusicDuration(e)} muted={false} onProgress={(e) => {setMusicCurrentTime(e.playedSeconds); setBufferDuration(e.loadedSeconds);}} />
+    <ReactPlayer style={{displasy: 'none'}} url={`https://music.youtube.com/watch?v=${TrackData.YTid}`} height={opts.height} width={opts.width} playing={isPlaying} controls={false} onBuffer={setIsBuffering(true)} onBufferEnd={setIsBuffering(false)} onDuration={(e) => setMusicDuration(e)} muted={false} onProgress={(e) => {setMusicCurrentTime(e.playedSeconds); setBufferDuration(e.loadedSeconds);}} />
   )
 })
 YTPlayer.displayName = 'YTPlayer'
