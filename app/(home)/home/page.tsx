021af3521f3cdb74740085eaaf29771d06fe7118
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-11 16:06:15 +0530

    section headings

--- app/(home)/home/page.tsx
+++ app/(home)/home/page.tsx
@@ -1,18 +1,23 @@
 import PlaylistPanel from '@/components/PlaylistPanel'
+import SectionHeading from '@/components/SectionHeading'
 import { FC } from 'react'
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
   return (
-    <div className="grid grid-cols-2 gap-2 w-full px-2 pt-2 mb-4">
-      <PlaylistPanel name='Discover' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
-      <PlaylistPanel name='Zombie' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
-      <PlaylistPanel name='Happy Mix' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
-      <PlaylistPanel name='Run This Town' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
-      <PlaylistPanel name='Skull Box' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
-      <PlaylistPanel name='Rock n Roll' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
-    </div>
+    <>
+      <div className="grid grid-cols-2 gap-2 w-full px-2 pt-2 mb-4">
+        <PlaylistPanel name='Discover' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
+        <PlaylistPanel name='Zombie' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
+        <PlaylistPanel name='Happy Mix' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
+        <PlaylistPanel name='Run This Town' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
+        <PlaylistPanel name='Skull Box' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
+        <PlaylistPanel name='Rock n Roll' image='https://lh3.googleusercontent.com/PZH8nrSG0_UE9ewl9nxelQn-19-mf6vyU0ddwsPZYJjdE2_05YkkIYfR9FCpzpC-CqTT9fQKtmP0Sw=w544-h544-l90-rj' />
+      </div>
+
+      <SectionHeading name='Made for You' />
+    </>
   )
 }
 
---
+++ components/SectionHeading.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+
+interface SectionHeadingProps {
+  name: string
+}
+
+const SectionHeading: FC<SectionHeadingProps> = ({ name }) => {
+  return (
+    <div className='flex items-center justify-between tracking-wider'>
+      <h1 className='pl-2 text-2xl text-white font-semibold'>{name}</h1>
+      <h2 className='pr-4 text-xs text-lightest self-end uppercase'>See All</h2>
+    </div>
+  )
+}
+
+export default SectionHeading
