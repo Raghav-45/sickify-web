c7e518dee2b6e372de4b21fb5d5743f917d94a99
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-11 15:56:06 +0530

    Update HomePageGreeting.tsx

--- components/HomePageGreeting.tsx
+++ components/HomePageGreeting.tsx
@@ -8,7 +8,7 @@ export const HomePageGreeting = () => {
   }
   return (
     // <div className='flex text-white h-auto w-full p-2 bg-gradient-to-b from-[#272727] to-dark/80 sticky top-0'>
-    <div className='flex text-white h-auto w-full p-2 sticky top-0'>
+    <div className='flex text-white h-auto w-full p-2 sticky inset-x-0 top-0'>
       <div className='flex-1 text-2xl font-bold align-middle items-center tracking-wide'>
         <h1>Good {greetByTime()}!</h1>
       </div>
