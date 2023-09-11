841de7e8908f48180d956654cd09c2e9f5a6d90b
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-11 15:54:32 +0530

    using next image

--- components/PlaylistPanel.tsx
+++ components/PlaylistPanel.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { FC } from 'react'
 
 interface PlaylistPanelsProps {
@@ -8,9 +9,12 @@ interface PlaylistPanelsProps {
 const PlaylistPanels: FC<PlaylistPanelsProps> = ({ name, image }) => {
   return (
     <div className='flex h-12 bg-light text-left text-xs align-middle items-center overflow-hidden rounded-md'>
-      <img
-        src={image}
+      <Image
+        height={36}
+        width={36}
         className='flex-none aspect-square h-full w-auto'
+        src={image}
+        alt={name}
       />
       <h1 className='flex-1 font-semibold text-white ml-2'>{name}</h1>
     </div>
