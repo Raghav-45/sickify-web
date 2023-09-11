045b21020758a307841e875cf1bc484262bd95fe
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-11 12:22:43 +0530

    player component design

--- app/(library)/library/page.tsx
+++ app/(library)/library/page.tsx
@@ -1,10 +1,15 @@
-import { Badge } from '@/components/ui/badge'
+import Player from '@/components/player'
 import { FC } from 'react'
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <p>rg</p>
+  return (
+    <>
+      <p>rg</p>
+      <Player />
+    </>
+  )
 }
 
 export default page
