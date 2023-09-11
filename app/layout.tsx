1c7a85bc84e4b1e4cb02c20cb50fd671638f331d
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-11 12:06:23 +0530

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,36 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Sickify',
   description: 'Where Music Gets Seriously Sick',
+  manifest: '/manifest.json',
+  icons: {
+    icon: '/icon-512x512.png',
+    shortcut: '/icon-512x512.png',
+    apple: [
+      { url: '/apple-icon.png' },
+      { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icon-256x256.png', sizes: '256x256', type: 'image/png' },
+      { url: '/icon-384x384.png', sizes: '384x384', type: 'image/png' },
+      { url: '/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+  },
+  themeColor: 'black',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  appleWebApp: {
+    title: 'Sickify',
+    statusBarStyle: 'black-translucent',
+    startupImage: [
+      '/icon-512x512.png',
+      {
+        url: '/icon-512x512.png',
+        media: '(device-width: 768px) and (device-height: 1024px)',
+      },
+    ],
+  },
+  category: 'music',
 }
 
 export default function RootLayout({
@@ -18,10 +48,7 @@ export default function RootLayout({
   return (
     <html
       lang='en'
-      className={cn(
-        'text-white bg-black antialiased light',
-        inter.className
-      )}
+      className={cn('text-white bg-black antialiased light', inter.className)}
     >
       <body className='min-h-screen bg-black antialiased'>
         <div className='container max-w-7xl mx-auto h-full px-0'>
