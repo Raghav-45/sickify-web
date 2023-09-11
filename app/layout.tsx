eabff85efb92537c0607111be10718ca2df1a6bf
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-11 12:33:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { Home, Search, Library } from 'lucide-react'
+import Player from '@/components/player'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -54,6 +57,15 @@ export default function RootLayout({
         <div className='container max-w-7xl mx-auto h-full px-0'>
           {children}
         </div>
+
+        <footer className='bg-gradient-to-t from-dark backdrop-blur-lg text-white fixed inset-x-0 bottom-0'>
+          <Player />
+          <div className='grid grid-cols-3 gap-0 h-14 mx-5 text-xl text-center'>
+            <Link href='/home' className='flex h-full w-full align-middle' replace><Home className='m-auto'/></Link>
+            <Link href='/search' className='flex h-full w-full align-middle' replace><Search className='m-auto'/></Link>
+            <Link href='/library' className='flex h-full w-full align-middle' replace><Library className='m-auto'/></Link>
+          </div>
+        </footer>
       </body>
     </html>
   )
