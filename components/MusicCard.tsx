43a82bef8b37bfc4b6f196dc36353db0649afda3
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-12 17:55:26 +0530

    Update MusicCard.tsx

--- components/MusicCard.tsx
+++ components/MusicCard.tsx
@@ -6,7 +6,7 @@ interface MusicCardProps {
   name: string
   artist: string | ArtistType[]
   image: string
-  onPlayButton?: any
+  onClick?: any
 }
 
 interface ArtistType {
@@ -25,18 +25,13 @@ function ArrayToStr(a: string | ArtistType[]): string {
   return ''
 }
 
-const MusicCard: FC<MusicCardProps> = ({
-  name,
-  artist,
-  image,
-  onPlayButton,
-}) => {
+const MusicCard: FC<MusicCardProps> = ({ name, artist, image, onClick }) => {
   return (
     <div className='relative flex-none group p-2 w-48'>
       <div className='bg-light w-full h-auto p-4 rounded-xl shadow-md'>
         <div className='relative aspect-square w-full shadow-[0_8px_24px_rgb(0,0,0,50%)] mb-3 overflow-hidden rounded-lg'>
           <div className='absolute playButton bg-green-500 rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
-            <button onClick={onPlayButton}>
+            <button onClick={onClick}>
               <Icons.Play className='text-white text-1xl' />
             </button>
           </div>
