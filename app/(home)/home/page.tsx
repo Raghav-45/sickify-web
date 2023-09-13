9b2086b5a43e1d14c1358bc5948451c0125cd61c
Aditya <raghavbhai4545@gmail.com>
2023-09-13 18:56:10 +0530

    Update page.tsx

--- app/(home)/home/page.tsx
+++ app/(home)/home/page.tsx
@@ -57,7 +57,9 @@ const page: FC<pageProps> = async ({}) => {
 
       <SectionHeading name='Made for You' />
       <div className='flex w-full hide-scrollbar overflow-x-scroll'>
+// @ts-ignore
         {data &&
+// @ts-ignore
           data.map((elem) => (
             <MusicCard
               key={elem.title}
@@ -67,8 +69,9 @@ const page: FC<pageProps> = async ({}) => {
             />
           ))}
       </div>
-
+// @ts-ignore
       {homepageData &&
+// @ts-ignore
         homepageData.map((section) => (
           <>
             <SectionHeading name={section.title} />
