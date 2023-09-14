bf49c23ec7d1d8cc3f77098151829c1683749a75
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-14 21:09:07 +0530

    react query

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,13 +1,18 @@
 'use client'
 
 import { FC, ReactNode } from 'react'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
 interface ProvidersProps {
   children: ReactNode
 }
 
+const queryClient = new QueryClient()
+
 const Providers: FC<ProvidersProps> = ({ children }) => {
-  return <>{children}</>
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
 }
 
 export default Providers
