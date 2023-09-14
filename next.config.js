b1d7afae9acd9a1b4419b333e925a5f559bc2f4d
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-14 22:12:23 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,11 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
+        hostname: '**',
       },
     ],
   },
