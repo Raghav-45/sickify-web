6f2db1810e30849f1051883e617af0a53f69d3f6
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-14 22:35:58 +0530

    Update page.tsx

--- app/(search)/search/page.tsx
+++ app/(search)/search/page.tsx
@@ -1,105 +1,11 @@
 'use client'
 
-import MusicCard from '@/components/MusicCard'
 import SearchBar from '@/components/SearchBar'
-import { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
 
 interface pageProps {}
 
-interface Album {
-  id: string
-  name: string
-}
-
-interface Artist {
-  id: string
-  name: string
-}
-
-interface Thumbnail {
-  height: number
-  url: string
-  width: number
-}
-
-interface Video {
-  id: string
-  videoType: string
-}
-
-interface FeedbackTokens {
-  add: null | any
-  remove: null | any
-}
-
-interface Song {
-  album: Album
-  artists: Artist[]
-  category: string
-  duration: string
-  duration_seconds: number
-  feedbackTokens: FeedbackTokens
-  isExplicit: boolean
-  resultType: string
-  thumbnails: Thumbnail[]
-  title: string
-  videoId: string
-  videoType: string
-  year: null | number
-}
-
-interface SongData {
-  album: Album
-  artists: Artist[]
-  isExplicit: boolean
-  thumbnails: Thumbnail[]
-  title: string
-  videoId: string
-}
-
-interface Content {
-  album?: Album
-  artists?: Album[]
-  isExplicit?: boolean
-  thumbnails: Thumbnail[]
-  title: string
-  videoID?: string
-  description?: string
-  playlistID?: string
-  views?: string
-  browseID?: string
-  year?: string
-}
-
-interface Homepage {
-  contents: SongData[]
-  title: string
-}
-
-const searchSong = async (q: string) => {
-  if (q.length == 0) return
-  const res = await fetch(
-    `https://sickify-web-api.vercel.app/search/songs?query=${q}`
-  )
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  return res.json()
-}
-
 const Page: FC<pageProps> = ({}) => {
-  const [searchQuery, setSearchQuery] = useState('')
-  const [searchResults, setSearchResults] = useState<Song[] | null>()
-
-  useEffect(() => {
-    let timerOut = setTimeout(() => {
-      searchSong(searchQuery).then((e) => {
-        setSearchResults(e)
-      })
-    }, 450)
-    return () => clearTimeout(timerOut)
-  }, [searchQuery])
-
   return <SearchBar />
 }
 
