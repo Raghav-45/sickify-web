4ddda1145fd8cfa7f657eef48ae79ebb01b362f1
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-15 18:38:54 +0530

    for playing music only on client side

---
+++ components/OverlayPlayButton.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { FC } from 'react'
+import { useGenerationStore } from './GenerationStore'
+
+interface OverlayPlayButtonProps {
+  name: string
+  artist: ArtistType[]
+  image: string
+  videoId: string
+}
+
+interface ArtistType {
+  id: string
+  name: string
+}
+
+const OverlayPlayButton: FC<OverlayPlayButtonProps> = ({
+  name,
+  artist,
+  image,
+  videoId,
+}) => {
+  const { setName, setArtist, setImage, setVideoId } = useGenerationStore()
+  return (
+    <div
+      onClick={() => {
+        setName(name)
+        setArtist(artist)
+        setImage(image)
+        setVideoId(videoId)
+      }}
+      className='absolute bg-green-400/10 h-full w-full top-0 inset-x-0'
+    ></div>
+  )
+}
+
+export default OverlayPlayButton
