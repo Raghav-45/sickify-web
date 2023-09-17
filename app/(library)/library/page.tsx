c294e05d1f9341f732b277eec8b32a620b71d76c
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-17 19:37:11 +0530

    Update page.tsx

--- app/(library)/library/page.tsx
+++ app/(library)/library/page.tsx
@@ -6,104 +6,23 @@ import Playlist from '@/components/Playlist'
 
 interface pageProps {}
 
-interface Album {
-  id: string
-  name: string
-}
-
-interface Artist {
-  id: string
-  name: string
-}
-
-interface Thumbnail {
-  height: number
-  url: string
-  width: number
-}
-
-interface Video {
-  id: string
-  videoType: string
-}
-
-interface FeedbackTokens {
-  add: null | any
-  remove: null | any
-}
-
-interface Song {
-  album: Album
-  artists: Artist[]
-  category: string
-  duration: string
-  duration_seconds: number
-  feedbackTokens: FeedbackTokens
-  isExplicit: boolean
-  resultType: string
-  thumbnails: Thumbnail[]
-  title: string
-  videoId: string
-  videoType: string
-  year: null | number
-}
-
-interface SongData {
-  album: Album
-  artists: Artist[]
-  isExplicit: boolean
-  thumbnails: Thumbnail[]
-  title: string
-  videoId: string
-}
-
-interface Content {
-  album?: Album
-  artists?: Album[]
-  isExplicit?: boolean
-  thumbnails: Thumbnail[]
-  title: string
-  videoID?: string
-  description?: string
-  playlistID?: string
-  views?: string
-  browseID?: string
-  year?: string
-}
-
-interface Homepage {
-  contents: SongData[]
-  title: string
-}
-
-const getData = async () => {
-  const res = await fetch(
-    'https://sickify-web-api.vercel.app/search/songs?query=arijit singh songs'
-  )
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  return res.json()
-}
+type PlaylistTypeWithId = PlaylistType & { id: string }
 
 async function getUserPlaylist() {
-  const data: (PlaylistType & { id: string })[] = []
+  const data: PlaylistTypeWithId[] = []
   const q = query(
     collection(db, 'playlists'),
     where('owner', '==', '81f07a37-d25d-474c-b029-e71e2fefc85b')
   )
   const querySnapshot = await getDocs(q)
   querySnapshot.forEach((doc) => {
-    data.push({ id: doc.id, ...doc.data() } as PlaylistType & { id: string })
-    console.log(doc.id, ' => ', doc.data())
+    data.push({ id: doc.id, ...doc.data() } as PlaylistTypeWithId)
   })
   return data
 }
 
 const page: FC<pageProps> = async ({}) => {
-  const data: SongData[] = await getData()
   const userPlaylists = await getUserPlaylist()
-
   return (
     <div>
       {userPlaylists &&
@@ -111,9 +30,7 @@ const page: FC<pageProps> = async ({}) => {
           <Playlist
             key={elem.name}
             name={elem.name}
-            image={
-              'https://lh3.googleusercontent.com/BF0qukRO1UhzY0I_0W4SyFUbwi24WaLalgbCSvH1vd1O-SKXZ3xSsNxw0OouObUEuRZ--xXVarYY3k4P=w120-h120-l90-rj'
-            }
+            image={'/icon-192x192.png'}
             id={elem.id}
           />
         ))}
