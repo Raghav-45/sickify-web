bf7ff95122f4283539a7830640e0c66ab19b234e
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-09-18 19:23:01 +0530

    Update OverlayPlayButton.tsx

--- components/OverlayPlayButton.tsx
+++ components/OverlayPlayButton.tsx
@@ -2,6 +2,9 @@
 
 import { FC } from 'react'
 import { useGenerationStore } from './GenerationStore'
+import AddToPlaylistButton from './AddToPlaylistButton'
+import { MoreVerticalIcon } from 'lucide-react'
+import { addToPlaylist } from '@/lib/dbUtils'
 
 interface OverlayPlayButtonProps {
   name: string
@@ -18,15 +21,30 @@ const OverlayPlayButton: FC<OverlayPlayButtonProps> = ({
 }) => {
   const { setName, setArtist, setImage, setVideoId } = useGenerationStore()
   return (
-    <div
-      onClick={() => {
-        setName(name)
-        setArtist(artist)
-        setImage(image)
-        setVideoId(videoId)
-      }}
-      className='absolute h-full w-full top-0 inset-x-0'
-    ></div>
+    <div className='absolute flex flex-row h-full w-full top-0 inset-x-0'>
+      <div
+        className='flex-auto h-full w-full'
+        onClick={() => {
+          setName(name)
+          setArtist(artist)
+          setImage(image)
+          setVideoId(videoId)
+        }}
+      ></div>
+      <div className='flex flex-none h-full w-auto aspect-square text-white items-center justify-center mr-1'>
+        <MoreVerticalIcon
+          className='h-5 w-5'
+          onClick={() => {
+            addToPlaylist('Zc1NENwJPxLFd9CoAJTP', {
+              name: name,
+              artist: artist,
+              image: image,
+              videoId: videoId,
+            })
+          }}
+        />
+      </div>
+    </div>
   )
 }
 
