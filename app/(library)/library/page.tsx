5d1cb6eeaab41f6c3f322b4a90390a8603e6b05b
Aditya <raghavbhai4545@gmail.com>
2024-03-06 22:00:03 +0530

    Update page.tsx

--- app/(library)/library/page.tsx
+++ app/(library)/library/page.tsx
@@ -1,23 +1,8 @@
-import { PlaylistType, PlaylistTypeWithId, getUserPlaylists } from '@/lib/dbUtils'
+import { getUserPlaylists } from '@/lib/dbUtils'
 import { FC } from 'react'
-import { collection, getDocs, query, where } from 'firebase/firestore'
-import { db } from '@/lib/firebaseClient'
 import Playlist from '@/components/Playlist'
 
 interface pageProps {}
-
-// async function getUserPlaylists() {
-//   const data: PlaylistTypeWithId[] = []
-//   const q = query(
-//     collection(db, 'playlists'),
-//     where('owner', '==', '81f07a37-d25d-474c-b029-e71e2fefc85b')
-//   )
-//   const querySnapshot = await getDocs(q)
-//   querySnapshot.forEach((doc) => {
-//     data.push({ id: doc.id, ...doc.data() } as PlaylistTypeWithId)
-//   })
-//   return data
-// }
 
 const page: FC<pageProps> = async ({}) => {
   const userPlaylists = await getUserPlaylists()
