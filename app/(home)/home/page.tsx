c860d87f59d0ee0c78483a5253640785b5e87dde
Aditya <raghavbhai4545@gmail.com>
2024-03-25 21:52:17 +0530

    Update page.tsx

--- app/(home)/home/page.tsx
+++ app/(home)/home/page.tsx
@@ -2,7 +2,6 @@ import MusicCard from '@/components/MusicCard'
 import PlaylistPanel from '@/components/PlaylistPanel'
 import SectionHeading from '@/components/SectionHeading'
 import { FC } from 'react'
-import {YTMUSIC as ytmusicapi} from 'ytmusic'
 
 interface pageProps {}
 
@@ -105,15 +104,9 @@ function ArrayToStr(a: string | Artist[]): string {
   return ''
 }
 
-const cookieyt = "YSC=rmtvUrAmk_s; VISITOR_INFO1_LIVE=A_qcuGtsSNQ; _gcl_au=1.1.284001967.1669825108; SID=RAiX890yck2ZvWf_PD69wG8vk63byHsiQiwa5DDP6SDFi9aWvA1sjw7M_JdXVxPh7iu7qA.; __Secure-1PSID=RAiX890yck2ZvWf_PD69wG8vk63byHsiQiwa5DDP6SDFi9aWToVgWw-qx7WzWTJ5OsqNhg.; __Secure-3PSID=RAiX890yck2ZvWf_PD69wG8vk63byHsiQiwa5DDP6SDFi9aWRYaM8QJUOkC8IV69vg1piA.; HSID=A0BzevEYY6d2ID_PP; SSID=A-qbFazKkieXx1sI2; APISID=r0dfpVeTcMC2cizH/ARoFJKXxYaKBtljSQ; SAPISID=laOXdKGfNe1SPpMK/Az0MU8uoc1fVSj4ur; __Secure-1PAPISID=laOXdKGfNe1SPpMK/Az0MU8uoc1fVSj4ur; __Secure-3PAPISID=laOXdKGfNe1SPpMK/Az0MU8uoc1fVSj4ur; LOGIN_INFO=AFmmF2swRAIgGfIY7SSA-INu9iV5nzdKhN69hT_iBcagiEFI5xcVwUUCIAQjcHE2VbNu4Xc9AoHdYRE3BBZjiMH8WqX3Hk3G0o5z:QUQ3MjNmeWxQWGFmTTJRbU1NbU80TG9feDBwRmZEaTVCTE9wZi1OZ3k4NjZRc3ZNM2xOOTktS3R0aFcyeGJIUmtSM05WTE1kSWFfcGNHOE5ONWFfX2tVVE9UYnZPZWdNa0t6YnF0S0R0N0J5bUhxazZFYlhQXzJVM3VLVW5qdmpSaUFZX1JyNVVmNTZZWEpDYngxUEQ2Zm54eXFpa1VXSUp3; SIDCC=AIKkIs1SmNiNq17VNfN5DXdAIrudOYX5EwbF5AeezUWUm7yH7jvQKGooWQ_L2JwG6Jdq3GA8ZQ; __Secure-1PSIDCC=AIKkIs0J71EStz4HIZCbIX59l_PLwRRqQwdePhcEhv6p6TL6SXSBts_O0V3dVZw9Vo8ybHgJPw; __Secure-3PSIDCC=AIKkIs0PpzxlZ5qsFXz0-M_gokno-g65cpnooCc2ZsDOa-2FIvZpN3QLC2El0zvw0QnNuIoQ; ST-jo7n95=csn=MC4xMzA4ODQwODQ0ODEyMzU5&itct=CA0QwJ4JGAAiEwjVhMzap9b7AhUIjtgFHfEWDDg%3D"
-
-const api = new ytmusicapi(cookieyt)
-
 const page: FC<pageProps> = async ({}) => {
-  // const data: Song[] = await getData()
-  // const homepageData: Homepage[] = await getHomePage()
-  const songs = await api.search('Hot stuff')
-  console.log(songs)
+  const data: Song[] = await getData()
+  const homepageData: Homepage[] = await getHomePage()
   return (
     <>
       <div className='gap-2 grid grid-cols-2 mb-4 px-2 pt-2 w-full'>
@@ -156,7 +149,7 @@ const page: FC<pageProps> = async ({}) => {
             />
           ))}
       </div> */}
-      {/* {homepageData &&
+      {homepageData &&
         homepageData.map((section) => (
           <>
             <SectionHeading name={section.title} />
@@ -172,7 +165,7 @@ const page: FC<pageProps> = async ({}) => {
               ))}
             </div>
           </>
-        ))} */}
+        ))}
     </>
   )
 }
