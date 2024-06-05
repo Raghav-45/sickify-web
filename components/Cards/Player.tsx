a78966e3fe008ce4455f8c2e449ff80894bbfdad
Aditya <raghavbhai4545@gmail.com>
2024-06-05 23:51:16 +0530

    Update Player.tsx

--- components/Cards/Player.tsx
+++ components/Cards/Player.tsx
@@ -11,6 +11,11 @@ interface PlayerProps {
 
 const Player: FC<PlayerProps> = ({ name, image, extra }) => {
   const [isPlaying, setIsPlaying] = useState(false)
+
+  const currentloaded = 35
+  const currentPlayed = 10
+  const trackLength = 100
+
   return (
     <View className="px-2 pt-2">
       <View className="relative flex flex-row h-14 w-full p-2 bg-white/10 align-middle items-center overflow-hidden rounded-xl backdrop-blur-lg transition-all">
@@ -30,21 +35,21 @@ const Player: FC<PlayerProps> = ({ name, image, extra }) => {
         <View className="flex flex-none flex-col order-last h-full justify-center items-center aspect-square">
           <TouchableOpacity onPress={() => setIsPlaying(!isPlaying)}>
             {isPlaying ? (
-              <FontAwesome6 size={18} name="play" color="white" />
-            ) : (
               <FontAwesome6 size={18} name="pause" color="white" />
+            ) : (
+              <FontAwesome6 size={18} name="play" color="white" />
             )}
           </TouchableOpacity>
         </View>
 
-        {/* <View
+        <View
           className="absolute h-[3px] bottom-0 left-0 mx-1 z-10 rounded-full bg-white/20 w-[0%] transition-all delay-0 duration-300 ease-in-out"
           style={{ width: `${(currentloaded / trackLength) * 100}%` }}
         ></View>
         <View
           className="absolute h-[3px] bottom-0 left-0 mx-1 z-20 rounded-full bg-green-500 w-[0%] transition-all delay-0 duration-300 ease-in-out"
           style={{ width: `${(currentPlayed / trackLength) * 100}%` }}
-        ></View> */}
+        ></View>
       </View>
     </View>
   )
